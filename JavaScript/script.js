6bdc9fff45f86b6232c18e87bef193aadd88d8d5
mimslr <loganmims52@gmail.com>
2023-07-07 21:41:37 -0500

    organized JS code

---
+++ JavaScript/script.js
@@ -0,0 +1,14 @@
+
+  // function to open HTML_email_pic
+  function openImage() {
+    var imageSrc = document.getElementById("HTML_email_pic").src;
+    window.open(imageSrc);
+  }
+  
+  function openImage2(){
+    var imageSrc2 = document.getElementById("Tundra_Trek_pic").src;
+    window.open(imageSrc2);
+  }
+  
+  
+  
